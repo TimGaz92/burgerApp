95c2b8efcfb4f197f2ce75bcc1abe371761a1cda
TimGaz92 <tgaziano@optonline.net>
2017-07-09 10:42:33 -0400

    changed folder structure, simplified app

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
 CREATE TABLE burgerTbl(
 id int auto_increment NOT NULL,
-name varchar(20) NOT NULL,
-eaten boolean, 
+name varchar(20),
 PRIMARY KEY (id)
 );
+
+insert into burgerTbl (name) VALUES ('test burger');
+insert into burgerTbl (name) VALUES ('big kahuna burger');
+insert into burgerTbl (name) VALUES ('Royale with cheese');
--- server.js
+++ server.js
@@ -1,15 +1,99 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var port = process.env.PORT || 3000;
+
 var app = express();
+var port = process.env.PORT || 3000;
+
 app.use(express.static("public"));
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-var routes = require("./controllers/controller.js");
-app.use("/", routes);
-app.listen(port);
-console.log("active on port " + port);
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db2"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Serve index.handlebars to the root route.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgerTbl;", function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log("select all");
+    res.render("index", { burgers: data });
+  });
+}); //functioning
+
+app.post("/", function(req, res) {
+  connection.query("INSERT INTO burgerTbl (name) VALUES (?)", [
+    req.body.name
+  ], function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log("insert");
+    res.redirect("/");
+  });
+}); //NOT FUNCTIONING
+
+app.delete("/:id", function(req, res) {
+  connection.query("DELETE FROM burgerTbl WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log("delete");
+    res.redirect("/");
+  });
+});//functioning 1002AM
+
+
+app.get("/:id", function(req, res) {
+  connection.query("SELECT * FROM burgerTbl where id = ?", [req.params.id], function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    console.log("select by id" + data);
+    res.render("single-burger", data[0]);
+  });
+});
+
+
+app.put("/:id", function(req, res) {
+  connection.query("UPDATE burgerTbl SET name = ? WHERE id = ?", [
+    req.body.name,  req.params.id
+  ], function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log("update by id functioning");
+    res.redirect("/");
+  });
+}); //this works - functioning 1002AM
+
+
+
+app.listen(port, function() {
+  console.log("active on: " + port);
+});
